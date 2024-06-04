b99c2e0ef8d61aa079d62f778ce74ec1b851d022
robherson <robhersonwector@gmail.com>
2024-06-04 16:34:51 -0300

    comit inicial

---
+++ Desafio-IBM-Brasil-Back/src/main/java/com/robherson/ibm/desafio/DesafioIbmBrasilApplication.java
@@ -0,0 +1,13 @@
+package com.robherson.ibm.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioIbmBrasilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioIbmBrasilApplication.class, args);
+	}
+
+}
