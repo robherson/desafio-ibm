009577d5d885f9656c044d7fd1a0866c8b5c71d3
robherson <robhersonwector@gmail.com>
2024-06-08 16:28:02 -0300

    add client.service

---
+++ Desafio-IBM-Frontend/src/app/services/client.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Client } from '../models/client.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientService {
+
+  private apiUrl = 'http://localhost:8080/api/clients';
+
+  constructor(private http: HttpClient) { }
+
+  getClients(): Observable<Client[]> {
+    return this.http.get<Client[]>(this.apiUrl);
+  }
+
+  getClientById(id: number): Observable<Client> {
+    return this.http.get<Client>(`${this.apiUrl}/${id}`);
+  }
+
+  addClient(client: Client): Observable<void> {
+    return this.http.post<void>(this.apiUrl, client);
+  }
+
+  updateClient(client: Client): Observable<void> {
+    return this.http.put<void>(`${this.apiUrl}`, client);
+  }
+
+  deleteClient(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
