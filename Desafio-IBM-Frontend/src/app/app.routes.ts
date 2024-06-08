e1fb8b667947e1a423a7f6eb4f0ec544c5945167
robherson <robhersonwector@gmail.com>
2024-06-08 16:09:29 -0300

    first commit front

---
+++ Desafio-IBM-Frontend/src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
+
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent }
+  ];
+
+  @NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
